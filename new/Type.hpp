0253d7ff369bb420915e53d6b9fbb035d4e4c95c
ctl <ctl.com>
2022-11-22 11:18:30 +0800

    成功将牌表耦合

--- new/Type.hpp
+++ new/Type.hpp
@@ -2,6 +2,7 @@
 #define _CTL_TYPE_HPP_
 
 #include "GroupCards.hpp"
+#include "Level.hpp"
 #include <map>
 #include <functional>
 
@@ -9,7 +10,7 @@ using std::map;
 using std::function;
 using std::cerr;
 
-using TypeType=unsigned int;
+using TypeType=unsigned short;
 enum TypeCards:TypeType{
     HIGH_CARD=1,ONE_PAIR,TWO_PAIRS,THREE_OF_A_KIND,STRAIGHT,
     FLUSH,FULL_HOUSE,FOUR_OF_A_KIND,STRAIGHT_FLUSH,ROYAL_FLUSH
@@ -18,27 +19,32 @@ enum TypeCards:TypeType{
 class Type;
 using Compare =function<bool(const Type &,const Type&)>;
 namespace GREADER{
-    bool KindCard(const Type&,const Type &);
+    /* bool KindCard(const Type&,const Type &); */
     // bool HighCard(const Type&,const Type &);
     // bool RoyalFlush(const Type&,const Type &);
 }//end of GREADER
 
 namespace EQUA{
-    bool Equa(const Type&,const Type &);
+    /* bool Equa(const Type&,const Type &); */
 }//end of EQUA
 
 namespace TYPE{
-    bool IsStraight(const Cards &cards);
-    bool IsRoyalFlush(const Cards &cards);
-    TypeType GetType(const Cards&cards5);
+    /* bool IsStraight(const Cards &cards); */
+    /* bool IsRoyalFlush(const Cards &cards); */
+    /* TypeType GetType(const Cards&cards5); */
+    LevelType GetLevelFromMap(const Cards&card5);
     void PrintType(TypeType type);
 
     // Compare SetGreadter(const TypeType type);
     Type GainType(Cards cards);
+    LevelType GainLevel(Cards cards);
     //根据公共牌返回对应的插入Cards排序
     void SwapCards(const Cards& pubcards,Cards& target);
     //查找contrust是否在pubcards中，如果是返回pos，不是返回-1；
     int FindCardInCards(const Cards& pubcards,const Card& contrust);
+
+    
+    
 }//end of TYPE
 
 
@@ -50,25 +56,23 @@ public:
     Type()
     :GroupCards(5)
     // ,_greadter(nullptr)
-    ,_type(0)
+    ,_level(0)
     {}
     Type(Cards cards5);
     Type(const Type &rhs)
         : GroupCards(5)
-        // , _greadter(rhs._greadter)
-        , _type(rhs._type)
+        , _level(rhs._level)
     {_cards = rhs._cards;}
     Type(Type &&rhs)
         : GroupCards(5)
-        // , _greadter(rhs._greadter)
-        , _type(rhs._type)
+        , _level(rhs._level)
     {_cards=rhs._cards;}
 
     Type &operator=(const Type &rhs){
         if(this!=&rhs){
             this->_cards = rhs._cards;
             // this->_greadter=rhs._greadter;
-            this->_type=rhs._type;
+            this->_level=rhs._level;
         }
         return *this;
    }
@@ -78,7 +82,7 @@ public:
        {
            this->_cards = rhs._cards;
         //    this->_greadter = rhs._greadter;
-           this->_type = rhs._type;
+           this->_level= rhs._level;
        }
        return *this;
    }
@@ -93,24 +97,21 @@ public:
      
     void Show()const;
     void ResetType(){
-        // _greadter=nullptr;
-        _type=0;
+        _level=0;
         _cards.clear();
     }
     void SetCards(Cards cards);
-    void SetType(TypeType type);
-    // void SetGreadter(){_greadter=TYPE::SetGreadter(_type);}
-    TypeType GetType(){return _type;}
+    void SetLevel(LevelType lv);
+    TypeType GetType()const{return LevelToType(_level);}
+    TypeType LevelToType(const LevelType)const;
 private:
     // Compare _greadter;
-    TypeType  _type;
+    LevelType _level;
 };
 
 
-//设置_type并获取对应的比较函数
-inline void Type::SetType(TypeType type){
-    _type=type;
-    // SetGreadter();
+inline void Type::SetLevel(LevelType lv){
+    _level=lv;
 }
 
 
--- new/main.cc
+++ new/main.cc
@@ -1,6 +1,6 @@
 #include "Game.hpp"
 
-
+Level* Level::_plevel=GetPlevel();//饿汉模式，开始就初始化
 
 int main(int argc, char const *argv[])
 {
@@ -9,6 +9,7 @@ int main(int argc, char const *argv[])
         cerr<<"argc !=2"<<endl;
         exit(1);
     }
+    Level::GetPlevel()->CreatMap();
     Game game(std::stoi(argv[1]));
     game.GameStart();
     return 0;
