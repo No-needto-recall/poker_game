7a72736e5d87fb383c10652807a94a9bd508d1e3
ctl <285>
2023-02-22 21:10:50 +0800

    清理

--- old/算法优化前/new/HandCards.cc
+++
@@ -1,5 +0,0 @@
-#include "HandCards.hpp"
-
-void HandCards::show()const{
-    GROUP_CARDS::PrintCards(*this,1,GetSize());
-}
--- old/算法优化前/new/PublicCards.cc
+++
@@ -1,5 +0,0 @@
-#include "PublicCards.hpp"
-
-void PublicCards::show()const{
-    GROUP_CARDS::PrintCards(*this,1,GetSize());
-}
